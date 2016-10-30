a00972add1468f073b71766b27110bdbcc8c02d0
Jonathan Palma <tanpalma04@gmail.com>
2016-10-30 17:58:33 -0600

    Charts are showing data on real time

--- project/public/js/app.js
+++ project/public/js/app.js
@@ -1,3 +1,29 @@
 /**
  * Created by palma on 10/30/16.
  */
+var app = angular.module("votingApp", ["chart.js"]);
+var controllers = {};
+
+app.config(['ChartJsProvider', function (ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+        responsive: true,
+        maintainAspectRatio: false
+    });
+}])
+
+
+controllers.chartCtrl = function ($scope, $interval) {
+    $scope.labels = ["Candidate 1", "Candidate 2", "Candidate 3"];
+    $scope.data = [300, 500, 100];
+
+    $interval(function () {
+        for(var i = 0; i < $scope.data.length; i++){
+            $scope.data[i] += Math.floor((Math.random() * 150) + 1);
+        }
+        console.log("data: " + JSON.stringify($scope.data));
+        $scope.$emit('$resize');
+    }, 5000);
+};
+
+app.controller(controllers);
